b15b0ae75500a1d0703b6a6bb22aeebb9c069df9
Charles Martins <phyns.br@gmail.com>
2022-07-02 18:19:21 -0300

    Added Env variables
    Need to finalize github actions to bring the secrets

--- Credentials.py
+++ Credentials.py
@@ -1,3 +1,3 @@
-import spotipy, tidalapi
+import spotipy, os
 
-SpotifyCredentials = spotipy.oauth2.SpotifyClientCredentials(client_id='83787093209b47549099c76467a13b72', client_secret='82e776e1dfef441eb6095307d45149d9')
+SpotifyCredentials = spotipy.oauth2.SpotifyClientCredentials(os.environ['CLIENT_ID_GITSECRET'], os.environ['CLIENT_SECRET_GITSECRET'])
