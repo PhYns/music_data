b21d2eb9cecaf79420fef8a2dff1f59dfbd8cf20
Charles Martins <phyns.br@gmail.com>
2022-07-02 18:05:05 -0300

    Create Credentials.py

---
+++ Credentials.py
@@ -0,0 +1,3 @@
+import spotipy, tidalapi
+
+SpotifyCredentials = spotipy.oauth2.SpotifyClientCredentials(client_id='83787093209b47549099c76467a13b72', client_secret='82e776e1dfef441eb6095307d45149d9')
