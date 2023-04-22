7d5f6140bfb5ccad996ac633f3cfb1fa8c92e761
Charles Martins <phyns.br@gmail.com>
2023-04-21 23:39:44 -0300

    created: getting spotify playlists form api

---
+++ playlist_spotify_to_ytmusic.py
@@ -0,0 +1,24 @@
+from services_sessions.spotify_service import spotify_session as spotify
+
+# TODO Create a class to convert playlists
+# TODO Treat errors
+
+# List all playlist and get ids
+playlists_items = dict()
+limit_user_playlists = 50
+offset_user_playlists = 0
+
+user_playlists_response = spotify.current_user_playlists(limit=limit_user_playlists, offset=offset_user_playlists)
+
+if user_playlists_response.get('error'):
+    print('There was an error in the user playlist request')
+else:
+    playlists_items = user_playlists_response['items'][0]
+    while user_playlists_response['next'] is not None:
+        offset_user_playlists += limit_user_playlists
+        user_playlists_response = spotify.current_user_playlists(limit=limit_user_playlists,
+                                                                 offset=offset_user_playlists)
+        if user_playlists_response.get('items'):
+            playlists_items = {**playlists_items, **user_playlists_response['items'][0]}
+
+print(playlists_items)
--- services_sessions/spotify_service.py
+++ services_sessions/spotify_service.py
@@ -6,5 +6,9 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 from services_sessions.credentials import spotify_credentials as credential
 
 # Create Spotify Session
-client_credentials = oauth2.SpotifyClientCredentials(client_id=credential.ClientId, client_secret=credential.ClientSecret)
-spotify_session = Spotify(client_credentials_manager=client_credentials)
+scope = ["user-library-read", "user-read-currently-playing", "playlist-modify-private", "user-follow-modify",
+         "user-library-modify", "playlist-read-private"]
+spotify_session = Spotify(auth_manager=oauth2.SpotifyOAuth(client_id=credential.ClientId,
+                                                           client_secret=credential.ClientSecret,
+                                                           redirect_uri="https://localhost/",
+                                                           scope=scope))
